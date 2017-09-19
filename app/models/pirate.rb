fba401578f15021075c25e8921b1c04a7484bde8
github username <github email address>
2017-09-19 11:16:38 -0400

    pirate lab

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+
+  @@all = []
+
+  def initialize(args)
+    #args = params[:pirate]
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
